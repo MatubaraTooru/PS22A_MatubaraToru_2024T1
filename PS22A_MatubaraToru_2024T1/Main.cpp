f2c96214f46e1868b75868b1985dcbc331550394
MatubaraTooru <106291581+MatubaraTooru@users.noreply.github.com>
2024-05-11 18:18:19 +0900

    ５月11日授業分

--- PS22A_MatubaraToru_2024T1/Main.cpp
+++ PS22A_MatubaraToru_2024T1/Main.cpp
@@ -1,96 +1,160 @@
 ﻿# include <Siv3D.hpp> // Siv3D v0.6.14
 
+namespace constants {
+	/// @brief ボール用定数
+	namespace ball {
+		/// @brief 半径
+		constexpr int RAD = 40;
+		/// @brief スピード
+		constexpr float SPEED = 250.0;
+	}
+
+	namespace brick {
+		constexpr Size SIZE{ 40, 20 };
+
+		constexpr int X_COUNT = 20;
+		constexpr int Y_COUNT = 5;
+		constexpr int MAX = X_COUNT * Y_COUNT;
+	}
+
+	namespace paddle {
+		constexpr Size SIZE{ 60, 10 };
+	}
+}
+
+class Ball {
+public:
+	/// @brief ボールの速度
+	Vec2 velocity;
+	/// @brief ボール
+	Circle ball;
+
+	Ball() : velocity({ 0, -constants::ball::SPEED }), ball({ 400, 400, 8 }) {}
+
+	void Draw() const {
+		ball.draw();
+	}
+
+	void Update() {
+		ball.moveBy(velocity * Scene::DeltaTime());
+	}
+};
+
+class Bricks {
+public:
+	Rect brickTable[ constants::brick::MAX ];
+
+	Bricks(){
+		using namespace::constants::brick;
+
+		for (int y = 0; y < Y_COUNT; ++y) {
+			for (int x = 0; x < X_COUNT; ++x)
+			{
+				int index = (y * X_COUNT) + x;
+
+				brickTable[index] = Rect{
+					x * SIZE.x,
+					60 + y * SIZE.y,
+					SIZE
+				};
+			}
+		}
+	}
+
+	void Draw() {
+		using namespace::constants::brick;
+
+		for (int i = 0; i < MAX; ++i) {
+			brickTable[i].stretched(-1).draw(HSV{ brickTable[i].y - 40 });
+		}
+	}
+
+	void Intersects(Ball* target) {
+		using namespace constants::brick;
+
+		// ブロックとの衝突を検知
+		for (int i = 0; i < MAX; ++i) {
+			auto& refBrick = brickTable[i];
+
+			if (refBrick.intersects(target->ball)) {
+
+				// ブロックの上辺、または底辺と交差
+				if (refBrick.bottom().intersects(target->ball) || refBrick.top().intersects(target->ball)) {
+					target->velocity.y *= -1;
+				}
+				else { // ブロックとの左辺、または右辺と交差
+					target->velocity.x *= -1;
+				}
+
+				// あたったブロックは画面外に出す
+				refBrick.y -= 600;
+
+				// 同一フレームでは複数のブロック衝突を検知しない
+				break;
+			}
+		}
+
+		// 天井との衝突
+		if ((target->ball.y < 0) && (target->velocity.y < 0)) {
+			target->velocity.y *= -1;
+		}
+
+		//　壁との衝突
+		if (((target->ball.x < 0) && (target->velocity.x < 0)) || ((Scene::Width() < target->ball.x) && (0 < target->velocity.x))) {
+			target->velocity.x *= -1;
+		}
+	}
+};
+
+class Paddle {
+public:
+	Rect paddle;
+
+	Paddle() : paddle({ Arg::center(Cursor::Pos().x, 500), constants::paddle::SIZE.x, constants::paddle::SIZE.y }) {}
+
+	void Draw() {
+		paddle.rounded(3).draw();
+	}
+
+	void Update() {
+		paddle.x = Cursor::Pos().x;
+	}
+
+	void Intersects(Ball* target) {
+		using namespace constants::ball;
+		// パドルとの衝突
+		if ((0 < target->velocity.y) && paddle.intersects(target->ball)) {
+			target->velocity = Vec2{
+				(target->ball.x - paddle.center().x) * 10,
+				-target->velocity.y,
+			}.setLength(SPEED);
+		}
+	}
+};
+
 void Main()
 {
-	// 背景の色を設定する | Set the background color
-	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
-
-	// 画像ファイルからテクスチャを作成する | Create a texture from an image file
-	const Texture texture{ U"example/windmill.png" };
-
-	// 絵文字からテクスチャを作成する | Create a texture from an emoji
-	const Texture emoji{ U"🦖"_emoji };
-
-	// 太文字のフォントを作成する | Create a bold font with MSDF method
-	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
-
-	// テキストに含まれる絵文字のためのフォントを作成し、font に追加する | Create a font for emojis in text and add it to font as a fallback
-	const Font emojiFont{ 48, Typeface::ColorEmoji };
-	font.addFallback(emojiFont);
-
-	// ボタンを押した回数 | Number of button presses
-	int32 count = 0;
-
-	// チェックボックスの状態 | Checkbox state
-	bool checked = false;
-
-	// プレイヤーの移動スピード | Player's movement speed
-	double speed = 200.0;
-
-	// プレイヤーの X 座標 | Player's X position
-	double playerPosX = 400;
-
-	// プレイヤーが右を向いているか | Whether player is facing right
-	bool isPlayerFacingRight = true;
+	Paddle paddle;
+	Ball ball;
+	Bricks bricks;
 
 	while (System::Update())
 	{
-		// テクスチャを描く | Draw the texture
-		texture.draw(20, 20);
+#pragma region 更新
+		paddle.Update();
+		ball.Update();
+#pragma endregion
 
-		// テキストを描く | Draw text
-		font(U"Hello, Siv3D!🎮").draw(64, Vec2{ 20, 340 }, ColorF{ 0.2, 0.4, 0.8 });
+#pragma region 衝突
+		paddle.Intersects(&ball);
+		bricks.Intersects(&ball);
+#pragma endregion
 
-		// 指定した範囲内にテキストを描く | Draw text within a specified area
-		font(U"Siv3D (シブスリーディー) は、ゲームやアプリを楽しく簡単な C++ コードで開発できるフレームワークです。")
-			.draw(18, Rect{ 20, 430, 480, 200 }, Palette::Black);
-
-		// 長方形を描く | Draw a rectangle
-		Rect{ 540, 20, 80, 80 }.draw();
-
-		// 角丸長方形を描く | Draw a rounded rectangle
-		RoundRect{ 680, 20, 80, 200, 20 }.draw(ColorF{ 0.0, 0.4, 0.6 });
-
-		// 円を描く | Draw a circle
-		Circle{ 580, 180, 40 }.draw(Palette::Seagreen);
-
-		// 矢印を描く | Draw an arrow
-		Line{ 540, 330, 760, 260 }.drawArrow(8, SizeF{ 20, 20 }, ColorF{ 0.4 });
-
-		// 半透明の円を描く | Draw a semi-transparent circle
-		Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1.0, 0.0, 0.0, 0.5 });
-
-		// ボタン | Button
-		if (SimpleGUI::Button(U"count: {}"_fmt(count), Vec2{ 520, 370 }, 120, (checked == false)))
-		{
-			// カウントを増やす | Increase the count
-			++count;
-		}
-
-		// チェックボックス | Checkbox
-		SimpleGUI::CheckBox(checked, U"Lock \U000F033E", Vec2{ 660, 370 }, 120);
-
-		// スライダー | Slider
-		SimpleGUI::Slider(U"speed: {:.1f}"_fmt(speed), speed, 100, 400, Vec2{ 520, 420 }, 140, 120);
-
-		// 左キーが押されていたら | If left key is pressed
-		if (KeyLeft.pressed())
-		{
-			// プレイヤーが左に移動する | Player moves left
-			playerPosX = Max((playerPosX - speed * Scene::DeltaTime()), 60.0);
-			isPlayerFacingRight = false;
-		}
-
-		// 右キーが押されていたら | If right key is pressed
-		if (KeyRight.pressed())
-		{
-			// プレイヤーが右に移動する | Player moves right
-			playerPosX = Min((playerPosX + speed * Scene::DeltaTime()), 740.0);
-			isPlayerFacingRight = true;
-		}
-
-		// プレイヤーを描く | Draw the player
-		emoji.scaled(0.75).mirrored(isPlayerFacingRight).drawAt(playerPosX, 540);
+#pragma region 描画
+		ball.Draw();
+		bricks.Draw();
+		paddle.Draw();
+#pragma endregion
 	}
 }
 
